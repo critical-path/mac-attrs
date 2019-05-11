cf3b68a839cd797f859c7c2548e862630bc5ceeb
critical-path <37241479+critical-path@users.noreply.github.com>
2019-05-11 17:53:23 -0400

    Update prep-mac-attrs.sh

--- prep-mac-attrs.sh
+++ prep-mac-attrs.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Fetch `oui.csv` from the IEEE's website.
+echo "Fetching 'oui.csv' from the IEEE's website..."
+echo
 curl http://standards-oui.ieee.org/oui/oui.csv > oui.csv
 
-# Fetch `cid.csv` from the IEEE's website. 
+echo "Fetching 'cid.csv' from the IEEE's website..."
+echo
 curl http://standards-oui.ieee.org/cid/cid.csv > cid.csv
 
-# Create, train, test, and save a classifier using `oui.csv` and `cid.csv`.
+echo "Creating, training, testing, and saving a classifier using 'oui.csv' and 'cid.csv'..."
+echo 
 python3 create_train_test_save.py
